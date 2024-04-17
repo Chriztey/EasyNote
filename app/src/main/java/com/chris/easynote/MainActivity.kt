daa30ea6615a5993b9b76b7d8337dee5ef55637d
Chriztey <CHRISTIETEYDADDY@GMAIL.COM>
2024-04-17 15:38:41 +0700

    adding adapter & view model

--- app/src/main/java/com/chris/easynote/MainActivity.kt
+++ app/src/main/java/com/chris/easynote/MainActivity.kt
@@ -2,10 +2,26 @@ package com.chris.easynote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.chris.easynote.adapter.NoteAdapter
+import com.chris.easynote.database.NoteDatabase
+import com.chris.easynote.repository.NoteRepository
+import com.chris.easynote.viewmodel.NoteViewModel
+import com.chris.easynote.viewmodel.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var noteViewModel : NoteViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupViewModel()
+    }
+
+    private fun setupViewModel() {
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application,noteRepository)
+        noteViewModel = ViewModelProvider(this, viewModelProviderFactory)[NoteViewModel::class.java]
     }
 }
